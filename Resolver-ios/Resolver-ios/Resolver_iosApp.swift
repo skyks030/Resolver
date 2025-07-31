1e1189e7368eca2468d05544014d2db2fe93c5a7
skyks030 <skymueller@icloud.com>
2025-08-01 01:06:42 +0200

    1.0.4

---
+++ Resolver-ios/Resolver-ios/Resolver_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resolver_iosApp.swift
+//  Resolver-ios
+//
+//  Created by Sky Müller on 01.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Resolver_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
