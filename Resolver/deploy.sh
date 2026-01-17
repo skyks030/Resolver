4bd8c3256bf47a5158e9de7ed400b8957d50b1d8
SHOOTnPOST <skymueller@tonbuero.com>
2026-01-17 21:43:18 +0100

    Fix: Resize DMG window to 512x512 to fill background

--- Resolver/deploy.sh
+++ Resolver/deploy.sh
@@ -91,14 +91,14 @@ tell application "Finder"
         set current view of container window to icon view
         set toolbar visible of container window to false
         set statusbar visible of container window to false
-        set the bounds of container window to {400, 100, 1000, 500}
+        set the bounds of container window to {400, 100, 912, 612}
         set theViewOptions to the icon view options of container window
         set arrangement of theViewOptions to not arranged
         set icon size of theViewOptions to 144
         set background picture of theViewOptions to file ".background:background.png"
         make new alias file at container window to file "Applications" with properties {name:"Applications"}
-        set position of item "Resolver" of container window to {160, 200}
-        set position of item "Applications" of container window to {440, 200}
+        set position of item "Resolver" of container window to {140, 256}
+        set position of item "Applications" of container window to {372, 256}
         close
         open
         update without registering applications
